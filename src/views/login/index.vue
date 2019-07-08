596b8d6a61b7f7799de9eaef31a2c73eb886e23a
Wsy-Xm <15097317238@163.com>
2019-07-08 22:35:18 +0800

    登陆页面结构样式修改

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,22 +1,25 @@
 <template>
   <div>
     <!-- 头部 -->
-    <van-nav-bar title="标题" />
+    <van-nav-bar title="登陆" />
     <!-- 账户密码输入框 -->
-    <form>
+    <form class="formBox">
       <van-cell-group>
         <van-field
           v-model="user.mobile"
           required
           clearable
-          label="用户名"
+          left-icon="phone-o"
           right-icon="question-o"
-          placeholder="请输入用户名"
-          @click-right-icon="$toast('question')"
+          placeholder="请输入手机号"
+          @click-right-icon="$toast('请输入输入号')"
         />
-        <van-field v-model="user.code" type="password" label="密码" placeholder="请输入密码" required />
+        <van-field v-model="user.code" center clearable  left-icon="comment-o" placeholder="请输入短信验证码">
+          <van-button slot="button" size="small" type="default" round>获取验证码</van-button>
+        </van-field>
+        <van-cell-group></van-cell-group>
       </van-cell-group>
-      <van-button type="info" @click.prevent="headleLogin">登陆</van-button>
+      <van-button class="login" type="info" @click.prevent="headleLogin">登陆</van-button>
     </form>
   </div>
 </template>
@@ -51,5 +54,11 @@ export default {
 <style lang="less" scoped>
 .van-button {
   width: 100%;
+}
+.formBox {
+  padding: 0 10px 0px 10px;
+  .login {
+    border-radius: 10px;
+  }
 }
 </style>
