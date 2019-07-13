2b3e9ee7d64f7f614e7b180d959396e34c49e601
Wsy-Xm <15097317238@163.com>
2019-07-13 16:05:29 +0800

    所有的频道列表封装加载和显示完成

--- src/api/channels.js
+++ src/api/channels.js
@@ -9,3 +9,14 @@ export const getUserChannels = () => {
     url: '/app/v1_0/user/channels'
   })
 }
+
+/**
+ * 获取全部频道列表
+ */
+
+export const getAllChannels = () => {
+  return request({
+    method: 'GET',
+    url: '/app/v1_0/channels'
+  })
+}
--- src/views/home/components/channel.vue
+++ src/views/home/components/channel.vue
@@ -1,38 +1,62 @@
 <template>
   <div>
+    <!-- 弹出层 -->
     <van-popup
       :value="value"
       @input="$emit('input',$event)"
       position="bottom"
       :style="{ height: '97%' }"
     >
-      <div class="headleContent">
-        <div class="mychannel">
-          我的频道
-          <small>点击进入频道</small>
+      <!-- 我的频道 -->
+      <div class="myChannelBox">
+        <div class="headleContent">
+          <div class="mychannel">
+            我的频道
+            <small>点击进入频道</small>
+          </div>
+          <van-button round type="danger" size="mini">编辑</van-button>
         </div>
-        <van-button round type="danger" size="mini">编辑</van-button>
+        <van-grid :gutter="10">
+          <van-grid-item v-for="(channelsItem,index) in channels" :key="channelsItem.id">
+            <span slot="text" :class="{active:active===index}" id="fzi">{{ channelsItem.name }}</span>
+          </van-grid-item>
+        </van-grid>
       </div>
-      <van-grid :gutter="10">
-        <van-grid-item v-for="(channelsItem,index) in channels" :key="channelsItem.id">
-          <span slot="text" :class="{active:active===index}" id="fzi">{{ channelsItem.name }}</span>
-        </van-grid-item>
-      </van-grid>
+      <!-- 推荐频道 -->
+      <div class="recommendChannel">
+        <van-grid :gutter="10">
+          <div class="recommend">
+            推荐频道
+            <small>点击添加频道</small>
+          </div>
+          <van-grid-item
+            v-for="allChannels in allChannels"
+            :key="allChannels.id"
+            :text="allChannels.name"
+          />
+        </van-grid>
+      </div>
     </van-popup>
   </div>
 </template>
 
 <script>
+// 加载所有的频道列表
+import { getAllChannels } from '@/api/channels'
+
 export default {
   name: 'channelPopup',
   props: {
+    // 控制弹出层
     value: {
       type: Boolean,
       default: false
     },
+    // 频道数据
     channels: {
       type: Array
     },
+    // 高亮处理
     active: {
       type: Number,
       default: 0
@@ -40,39 +64,70 @@ export default {
   },
   data() {
     return {
-      // show: true
+      allChannels: []
     }
   },
-  mounted() {},
-  methods: {}
+  created() {
+    // 加载所有的频道列表
+    this.loadAllchannels()
+  },
+  methods: {
+    // 加载所有的频道列表
+    async loadAllchannels() {
+      try {
+        const data = await getAllChannels()
+        this.allChannels = data.channels
+        console.log(data)
+      } catch (err) {
+        console.log(err)
+        console.log('全部频道列表加载失败')
+      }
+    }
+  }
 }
 </script>
 
 <style lang="less" scoped>
-.mychannel {
-  padding: 20px;
-  font-size: 30px;
-  small {
-    font-size: 12px;
-    color: #ccc;
+.myChannelBox {
+  .mychannel {
+    padding: 20px;
+    font-size: 30px;
+    small {
+      font-size: 12px;
+      color: #ccc;
+    }
   }
-}
-.van-grid-item /deep/ .van-grid-item__content {
-  background: #eee;
-}
-.headleContent {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  .van-button--danger {
-    background: #fff;
+  .headleContent {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .van-button--danger {
+      background: #fff;
+      color: red;
+    }
+  }
+  .active {
     color: red;
   }
+  #fzi {
+    font-size: 12px;
+  }
+  .van-grid-item /deep/ .van-grid-item__content {
+    background: #eee !important;
+  }
 }
-.active {
-  color: red;
-}
-#fzi {
-  font-size: 12px;
+// .van-grid-item /deep/ .van-grid-item__content {
+//   background: #eee;
+// }
+.recommendChannel {
+  .recommend {
+    width: 100%;
+    padding: 20px;
+    font-size: 30px;
+    small {
+      font-size: 12px;
+      color: #ccc;
+    }
+  }
 }
 </style>
